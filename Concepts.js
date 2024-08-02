1c88e3db365f8f32fa154aaea18d5760a67349a9
andiboshtrakaj <121944256+andiboshtrakaj@users.noreply.github.com>
2024-08-02 10:34:52 +0200

    Add files via upload

---
+++ Concepts.js
@@ -0,0 +1,25 @@
+function Concepts(props) {
+    return (
+        
+      <ul id="concepts">
+        <li className="concept">
+          <img src={props.items[0].image} alt={props.items[0].title} />
+          <h2>{props.items[0].title}</h2>
+          <p>{props.items[0].description}</p>
+        </li>
+        <li className="concept">
+          <img src={props.items[1].image} alt={props.items[1].title} />
+          <h2>{props.items[1].title}</h2>
+          <p>{props.items[1].description}</p>
+        </li>
+        <li className="concept">
+          <img src={props.items[2].image} alt={props.items[2].title} />
+          <h2>{props.items[2].title}</h2>
+          <p>{props.items[2].description}</p>
+        </li>
+      </ul>
+    );
+  }
+  
+  export default Concepts;
+  
